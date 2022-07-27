8a72785ba5e0a1e389f843798180c869db736005
juliusdc14 <juliusdc05@gmail.com>
2022-07-27 14:44:13 -0500

    finished Methods

---
+++ MethodOverload/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MethodOverload
+{
+    class Program
+    {
+        public static int Add (int num1 , int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add (decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add (int num1, int num2, bool myMoney)
+        {
+            var sum = num1 + num2;
+
+            if (myMoney == true && sum > 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if(myMoney == true && sum == 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if(myMoney == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+    }
+}
